318f097d8acebb8e18d253920878a9fef5b2274b
Trish Ogen <patriciaogen@gmail.com>
2019-07-31 23:10:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Trish."
+  end
+
+  get '/hometown' do
+    "My hometown is Montreal."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Uptown Girl."
+  end
 end
